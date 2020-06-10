10f530db5bcd4c63c5f2fc353a739fc252b4df0a
beaniegirl <40733719+beaniegirl@users.noreply.github.com>
2020-06-10 14:03:13 +0530

    Update submit.php

---
+++ Form/submit.php
@@ -0,0 +1,15 @@
+<form action=”monis.rebeca@gmail.com”
+
+method=”POST”
+
+enctype=”multipart/form-data”
+
+name=”EmailForm”>
+
+Name:<br>
+
+<input type=”text” size=”19″ name=”ContactName”><br><br>
+
+Message:<br> <textarea name=”ContactCommentt” rows=”6″ cols=”20″>
+
+</textarea><br><br> <input type=”submit” value=”Submit”> </form>
